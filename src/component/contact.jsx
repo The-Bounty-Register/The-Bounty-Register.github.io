64181b1764b925cd48cb653c31f1b23429776366
Mohamed Yusuf Mohamed Javid <mohdgazyanii235@gmail.com>
2024-08-15 22:46:14 +0100

    made contact-us form functional

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import './ContactForm.css';
 import Header from './header';
 
 const ContactPage = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    company: '',
+    message: ''
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://formspree.io/f/xovalebg', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      
+      if (response.ok) {
+        setSubmitted(true);
+        setFormData({
+          name: '',
+          email: '',
+          phone: '',
+          company: '',
+          message: ''
+        });
+        setTimeout(() => setSubmitted(false), 5000);
+      } else {
+        console.error('Error submitting form');
+      }
+
+    } catch (error) {
+      console.error('Error submitting form', error);
+    }
+  };
+
+
+
   return (
     <>
       <Header />
@@ -37,25 +89,39 @@ const ContactPage = () => {
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 1, delay: 0.5 }}
         >
-          <form>
+          <form name='contactus-form' onSubmit={handleSubmit}>
             <label htmlFor="name">name</label>
-            <input type="text" id="name" name="name" placeholder="full name" />
+            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} placeholder="full name" />
 
             <label htmlFor="email">email address</label>
-            <input type="email" id="email" name="email" placeholder="email" />
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} placeholder="email" />
 
-            <label htmlFor="reason">how can we help?</label>
-            <input type="text" id="help" name="help" placeholder="how can we help?" />
+            <label htmlFor="phone">phone number</label>
+            <input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} placeholder="phone number" />
 
-            <input type="file" id="file" name="file" className="file-input" />
-            <label htmlFor="file" className="file-label">
-              attach or submit relevant files
-            </label>
+            <label htmlFor="company">Company</label>
+            <input type="text" id="company" name="company" value={formData.company} onChange={handleChange} placeholder="company name" />
+
+            <label htmlFor="message">message</label>
+            <textarea rows="4" cols="50" name="message" form="contactus-form" value={formData.message} onChange={handleChange} className='message' placeholder='How can we help?'></textarea>
 
             <button type="submit">Submit</button>
           </form>
         </motion.div>
       </motion.div>
+
+      {submitted && (
+        <motion.div
+          className="popup"
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <h2>We will be in touch soon!</h2>
+          <div className="loader"></div>
+        </motion.div>
+      )}
+
     </>
   );
 };
